eeda7bca3cc814d8b4360d91796a93fea10cb23d
Patrick Mazulo <pmazulo@gmail.com>
2020-07-22 22:43:03 -0300

    Finalizada parte 4 do tutorial

--- polls/urls.py
+++ polls/urls.py
@@ -6,14 +6,9 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
